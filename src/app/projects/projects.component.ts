ccafd0d99e6dc9f9dc015370c208057dcaf6588b
Marco Satti <marcosatti@gmail.com>
2020-11-04 22:14:19 +0800

    Website work

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { ProjectsService } from '../common/services/projects.service';
 import { ProjectItem } from '../common/types/project';
 
@@ -10,25 +8,23 @@ import { ProjectItem } from '../common/types/project';
     styleUrls: ['projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-    projects$: Observable<ProjectItem[]>;
+    projects: ProjectItem[] | null = null;
 
     constructor(private projectsService: ProjectsService) {
     }
 
     ngOnInit(): void {
-        this.projects$ = this.projectsService.getProjects().pipe(
-            map((projects) => {
-                return projects.sort((project1, project2) => {
-                    // Sorted descending order.
-                    if (project1.lastUpdated > project2.lastUpdated) {
-                        return -1;
-                    } else if (project1.lastUpdated < project2.lastUpdated) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                });
-            })
-        );
+        this.projectsService.getProjects().subscribe((projects) => {
+            this.projects = projects.sort((project1, project2) => {
+                // Sorted descending order.
+                if (project1.lastUpdated > project2.lastUpdated) {
+                    return -1;
+                } else if (project1.lastUpdated < project2.lastUpdated) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+        });
     }
 }
