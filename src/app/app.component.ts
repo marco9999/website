34776ebb1e6e7347dfa6122153f5660308ef380b
Marco Satti <marcosatti@gmail.com>
2021-01-26 14:26:44 +0800

    Try a locale fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID } from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    constructor(@Inject(LOCALE_ID) locale: string) {
+        console.log(`Locale is ${locale}`);
+    }
 }
